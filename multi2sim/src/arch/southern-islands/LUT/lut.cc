7c4a5212ea2a1507d68a25b78bd7c784e0b3622a
jaguar1700AD <shashanksharma0309@gmail.com>
2020-08-18 08:21:14 +0000

    .gitignore files removed

--- multi2sim/src/arch/southern-islands/LUT/lut.cc
+++ multi2sim/src/arch/southern-islands/LUT/lut.cc
@@ -8,37 +8,65 @@ lut::lut(unsigned int size, unsigned int epsilon)
 	num_misses = 0;
 }
 
-set<lut::value>::iterator lut::get_closest(value V)
+set<lut::value>::iterator lut::get_closest(elem V)
 {
 	if (table.size() == 0) return table.end();
-
-	auto it = table.lower_bound(V);
-	if (it == table.begin()) return it;
-	auto it1 = prev(it);
-
-	if (abs_diff(*it, V) < abs_diff(*it1, V)) return it;
-	else return it1;
+	
+	auto candidate = table.begin();
+	for(auto it = table.begin(); it != table.end(); it++)
+	{
+		if (abs_diff(*it, V) < abs_diff(*candidate, V)) candidate = it;
+	}
+	return candidate;
 }
 
-bool lut::find(unsigned int V, unsigned int &store)
+set <lut::value>::iterator lut::get_LRU()
 {
-	value val; val.as_uint = V;
+	return table.begin();
+}
+
+void lut::change_elem_time(set<elem>::iterator it, unsigned int new_time)
+{
+	elem e = *it; e.last_used = new_time;
+        table.erase(it);
+        table.insert(e);
+}
+
+void lut::make_old()
+{
+	for(auto it = table.begin(); it != table.end(); it++) 
+	{
+		elem e = *it; e.last_used += 1;
+       		table.erase(it);
+       		table.insert(it, e); // Hint provided for fast O(1) insertion
+	}
+}
+
+bool lut::find(unsigned int a, unsigned int b, unsigned int &store1, unsigned int &store2)
+{
+	elem val = new elem(a, b, 0);
 	auto it = get_closest(val);
 	
 	if (it == table.end() || abs_diff(*it, val) > epsilon)
 	{
 		// Miss
 		
-		if (table.size() == size) table.erase(it);
-		table.insert(val);
+		if (table.size() == size) table.erase(table.get_LRU());
+		table.insert(table.end(), val);
 		num_misses += 1;
+		
+		make_old();
+		recent_hit = false;
 		return false; 
 	}
 	
 	// Hit	
-
 	num_hits += 1;
-	store = (*it).as_uint;
+	change_elem_time(it, 0);
+
+	make_old();	
+	recent_hit = true;
+	store1 = (*it).as_uint[0]; store2 = (*it).as_uint[1];
 	return true;
 }
 
--- multi2sim/src/arch/southern-islands/LUT/lut.h
+++ multi2sim/src/arch/southern-islands/LUT/lut.h
@@ -7,26 +7,37 @@ class lut
 {
 	union value
 	{
-		unsigned int as_uint;
-		int as_int;
+		unsigned int as_uint[2];
+		int as_int[2];
 	};
 	
-	struct classcomp 
+	class elem
 	{
-  		bool operator() (value v1, value v2) { return v1.as_uint < v2.as_uint; }
+	public:
+		value data; unsigned int last_used;
+		elem(unsigned int a, unsigned int b, unsigned int last_used)
+		{
+			data.as_uint = {a, b};
+			this->last_used = last_used;
+		}
 	};
 	
-	
-	set <value, classcomp> table;
+	struct comp {bool operator() (elem e1, elem e2) {return e1.last_used > e2.last_used; } };	
+
+	set <elem, comp> table;
 	unsigned int size;	
 	unsigned int epsilon;
 
 	unsigned long int num_hits;
 	unsigned long int num_misses;
 	
-	unsigned int abs_diff(value v1, value v2) { return abs(v1.as_int - v2.as_int); }
+	bool recent_hit; // True if most recent access was a hit
+	
+	unsigned int abs_diff(elem v1, elem v2) { return abs(v1.data.as_int[0] - v2.data.as_int[0]) + abs(v1.data.as_int[1] - v2.data.as_int[1]); }
 	set<value>::iterator get_closest(value V);
-	void print_value(set<value>::iterator it) { cout << (*it).as_uint << endl;}
+	set<value>::iterator get_LRU();
+	void make_old(); // Increase the last_used of all elements by 1
+	void print_value(set<value>::iterator it) { cout << "Data: " << (*it).data.as_uint[0] << ", " << (*it).data.as_uint[1] << " Last Used: " << (*it).last_used << endl;}
 
 	
 
@@ -35,7 +46,7 @@ public:
 	lut(unsigned int size, unsigned int epsilon);
 	
 	// Return true and closest value for a hit	
-	bool find(unsigned int V, unsigned int &store);
+	bool find(unsigned int a, unsigned int b, unsigned int &store1, unsigned int &store2);
 
 	void print();
 
@@ -43,5 +54,6 @@ public:
 	unsigned long int get_num_hits() { return num_hits; }
 	unsigned long int get_num_misses() { return num_misses; }
 	unsigned long int get_num_ops() { return num_hits + num_misses; }
+	bool get_recent_hit() {return recent_hit; }
 
 };
